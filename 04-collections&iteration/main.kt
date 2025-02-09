2369897e8e1e6c35f5e2dd54aacb45cf9da54309
malybaryl <kacper_barylowicz@wp.pl>
2025-02-09 02:28:43 +0100

    Containers&Iteration

--- 04-collections&iteration/main.kt
+++ 04-collections&iteration/main.kt
@@ -39,4 +39,75 @@ fun main(){
     intrestingThings.forEach { intrestedThing ->    //* Changing the default parameter name
         println(intrestedThing)
     }
+
+    //? Why we write forEach without ()?
+    // It is cause it is a lambda expression
+
+    println("\n---------\n")
+
+    // Iterating with index
+    intrestingThings.forEachIndexed { index, intrestedThing ->
+        println("Item at $index is $intrestedThing")
+    }
+
+
+    println("\n---------\n")
+
+    // LIST
+
+    val list = listOf("a", "b", "c", "d", "e")
+    println(list[0])
+    println(list.get(0))
+
+    intrestingThings.forEach { intrestedThing ->
+        println("$intrestedThing")
+    }
+
+    println("\n---------\n")
+
+    val mutableList = mutableListOf("a", "b", "c", "d", "e")
+    mutableList.add("f")
+    mutableList.removeAt(0)
+    mutableList.forEach { intrestedThing ->
+        println("$intrestedThing")
+    }
+
+    // ARRAY VS LIST
+    // Key Differences:
+    // ----------------------------------------------------------------------------------------
+    // | Feature         | Array                        | List (Immutable)   | MutableList     |
+    // ----------------------------------------------------------------------------------------
+    // | Size           | Fixed                        | Dynamic            | Dynamic         |
+    // | Modification   | Allowed (elements only)     | Not allowed        | Allowed         |
+    // | Performance    | Fast access (indexed)       | Fast access        | Slightly slower |
+    // | Resizing      | Not possible                 | Not possible       | Possible        |
+    // ----------------------------------------------------------------------------------------
+
+    // Maps
+
+    val map = mapOf("a" to 1, "b" to 2, "c" to 3)
+    println(map["a"])
+
+    println("\n---------\n")
+
+    map.forEach { (key, value) ->
+        println("$key -> $value")
+    }
+
+    val mutableMap = mutableMapOf("a" to 1, "b" to 2, "c" to 3)
+    mutableMap["d"] = 4
+    mutableMap.remove("a")
+    mutableMap.forEach { (key, value) ->
+        println("$key -> $value")
+    }
+
+    println("\n---------\n")
+
+    fun hello(greeting: String, target: List<String>) {
+        target.forEach { name ->
+            println("$greeting, $name!")
+        }
+    }
+
+    hello("Hello", intrestingThings)
 }
